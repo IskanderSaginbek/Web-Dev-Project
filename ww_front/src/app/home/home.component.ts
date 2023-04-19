d89d3e6a221a6a4e58128d624a6d02c12f1a4a61
IskanderSaginbek <saginbekiskander@gmail.com>
2023-04-20 01:09:28 +0600

    few small tweaks

--- ww_front/src/app/home/home.component.ts
+++ ww_front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {category} from "../../interfaces";
+import {Constants} from "../app.component";
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  categories : category[] = Constants.cats.slice(0,4);
   constructor() { }
 
   ngOnInit(): void {
