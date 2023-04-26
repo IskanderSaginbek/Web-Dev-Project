72eefec74500535d822adb55628def58f5612cae
IskanderSaginbek <saginbekiskander@gmail.com>
2023-04-26 14:59:57 +0600

    added a somewhat unappealing share button

--- ww_front/src/app/product/product.component.ts
+++ ww_front/src/app/product/product.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {faFilePdf, faShareAlt, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {faFilePdf, faLink, faShareAlt, faTimes, faUserCircle} from "@fortawesome/free-solid-svg-icons";
 import {Constants} from "../app.component";
 import {user,shipment,product,category,cart_item,mfr} from "../../interfaces"
 import {ActivatedRoute} from "@angular/router";
+import {faFacebook, faTelegram, faWhatsapp} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-product',
@@ -31,6 +32,12 @@ export class ProductComponent implements OnInit {
   fapdf = faFilePdf;
   fauser = faUserCircle;
   faShare = faShareAlt;
+  faFB = faFacebook;
+  faTG = faTelegram;
+  faWA = faWhatsapp;
+  faChain = faLink;
+  faX = faTimes;
+  share_vis : string = "none";
   constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -41,4 +48,29 @@ export class ProductComponent implements OnInit {
     this.m = Constants.mfrs[this.p.mfr_id];
   }
 
+  share(site : number): void {
+    switch(site) {
+      case 0 : {
+        window.open("https://t.me/share/url?url="+window.location.href+"&text=Look what I've found on wiredworld.com","_blank")
+        break;
+      }
+      case 1 : {
+        window.open("https://api.whatsapp.com/send?text=\""+window.location.href+"\"","_blank")
+        break;
+      }
+      case 2 : {
+        window.open("http://www.facebook.com/dialog/send?app_id=123456789&"+window.location.href,"_blank")
+        break;
+      }
+      case 3 : {
+        navigator.clipboard.writeText(window.location.href);
+        alert("Copied to clipboard: " + window.location.href);
+        break;
+      }
+    }
+  }
+  toggle_share() : void {
+    if (this.share_vis=="none") this.share_vis = "flex";
+    else this.share_vis = "none";
+  }
 }
