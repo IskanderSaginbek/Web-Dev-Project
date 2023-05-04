dc611508cb458adcb0fae206f204edaccd731945
IskanderSaginbek <98901249+IskanderSaginbek@users.noreply.github.com>
2023-05-04 12:53:16 +0600

    Update settings.py

--- ww_back/project/project/settings.py
+++ ww_back/project/project/settings.py
@@ -155,16 +155,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "12345678",
-#         "HOST": "localhost",
-#         "PORT": "", # 5432
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': "postgres",
+         "USER": "postgres",
+         "PASSWORD": "12345678",
+         "HOST": "localhost",
+         "PORT": "", # 5432
+     }
+ }
 
 DATABASES = {
     'default': {
